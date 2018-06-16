d0ec85013480f0f4aedf619d8ff204869d5ef9b8
Filip Stepien <contact@filipstepien.com>
2018-06-16 13:23:40 -0400

    resolve git merge conflic... add mika secret code, hojar tracking'

--- scripts/app.js
+++ scripts/app.js
@@ -16,10 +16,26 @@ function submitForm(e) {
   //   console.log(rawData);
   data = JSON.stringify(rawData);
   let formSend = new XMLHttpRequest();
-  formSend.open("POST", "https://formspree.io/mdzewapm", true);
+  formSend.open("POST", "https://formspree.io/mgykdpvm", true);
   formSend.setRequestHeader("Content-type", "application/json");
   formSend.send(data);
 
   document.querySelector(".alert").style.display = "block";
   document.getElementById("contact-form").reset();
+}
+
+/////"Konami Code"
+const pressed = [];
+const konamicode = "whippet";
+window.addEventListener("keyup", e => {
+  pressed.push(e.key);
+  pressed.splice(-konamicode.length - 1, pressed.length - konamicode.length);
+  pressed.join("").includes(konamicode) ? konamiMatch() : "";
+});
+
+function konamiMatch() {
+  const photo = document.querySelector(".about__wrap__photo");
+  photo.style.backgroundImage = "url(./../assets/mika.jpg)";
+  photo.style.filter = "grayscale(0%) brightness(1)";
+  window.location.href = "#about";
 }
